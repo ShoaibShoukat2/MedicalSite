43bd2b6a2fbb265fbc76547d907d9d831b91d9d2
shoaib <shoaibshoukatbhatti99@gmail.com>
2025-02-21 00:56:22 +0500

    add changes

--- main/settings.py
+++ main/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "corsheaders",
     'frontend',
     'user_account',
     'patientdashboard',
